fc584d1c2f87a8cf55d4e1a27c0ea088a1ca639e
Carlota <caritoloustau@gmail.com>
2025-08-30 13:19:06 -0300

    Se crea la carpeta factory junto  a las clases: ConnectionManager, DaoFactory y DBType

---
+++ Integrador1/src/main/java/factory/DaoFactory.java
@@ -0,0 +1,35 @@
+package factory;
+
+public abstract class DaoFactory {
+    private static volatile DaoFactory instance;
+    public static DaoFactory getInstance(DBType type){
+        if (instance==null){
+            synchronized (DaoFactory.class){
+                if (instance==null){
+                    switch (type){
+                        case MYSQL:
+                            instance = new MySQLDAOFactory();
+                            break;
+                        default:
+                            throw new IllegalArgumentException("DTBType no soportado: " + type);
+                    }
+                }
+            }
+
+        }
+        return instance;
+    }
+
+    public static DaoFactory getInstance(){
+        String v = System.getProperty("db.type", "MYSQL");
+        DBType type = DBType.valueOf(v.toUpperCase());
+        return getInstance(type);
+
+    }
+
+   // public abstract ClienteDAO createClienteDAO();
+    // public abstract FacturaDAO createFacturaDAO();
+   // public abstract Factura_ProductoDAO createFacturaProductoDAO();
+   // public abstract ProductoDAO crateProductoDAO();
+
+}
